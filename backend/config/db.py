a28dcc2712901b5d8b709b055ea673406f6d8ed0
NehaS101 <solankineha297@gmail.com>
2023-07-19 20:49:53 +0530

    updated db.py

---
+++ backend/config/db.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+
+mongo=None
+
+def connection(connection):
+    global mongo
+    try:
+        mongo_url = "mongodb+srv://solankineha297:prismport@cluster0.vauaqjy.mongodb.net/prism?retryWrites=true&w=majority"
+        mongo=MongoClient(mongo_url)
+        print("connected to db")
+    except Exception as e:
+        print("Error connecting to db")    
