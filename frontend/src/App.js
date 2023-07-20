6f9ac1b762e3cbc6b04c179db6a67f33190a92d8
NehaS101 <solankineha297@gmail.com>
2023-07-20 22:00:58 +0530

    router's route done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {BrowserRouter as Router,Route,Link,Switch} from 'react-router-dom'
+
 
 function App() {
+  
   return (
+    <Router>
     <div className="App">
-      
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/portfolio-managers">Portfolio Managers</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to ="/tasks">Tasks</Link>
+          </li>
+          <li>
+            <Link to="/resources">Resources</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <Route exact path="/">
+
+        </Route>
+        <Route exact path="/portfolio-managers">
+          <ManagerComponent />
+        </Route>
+        <Route exact path="/projects">
+          <ProjectComponent />
+        </Route>
+        <Route exact path="/tasks">
+          <TaskComponent />
+        </Route>
+        <Route exact path="/resources">
+          <ResourceComponent />
+        </Route>
+        <Route exact path="/login">
+          <LoginComponent />
+        </Route>
+        <Route exact path="/signup">
+          <SignupComponent />
+        </Route>
+      </div>
     </div>
+  </Router>
   );
 }
 
---
+++ frontend/src/components/managercomponent.js
@@ -0,0 +1,1 @@
+import REact,{useEffect,useState} from 'react';
