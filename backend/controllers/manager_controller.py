c5574c13c758db9bda40ed6fa44a60223d5819d1
NehaS101 <solankineha297@gmail.com>
2023-07-20 14:20:47 +0530

    update db in manager_controller

--- backend/controllers/manager_controller.py
+++ backend/controllers/manager_controller.py
@@ -1,2 +1,18 @@
 from flask import request, jsonify
+import sys
+import os
 
+# Add the parent_folder to the Python path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from config.db import db
+
+def create_managers():
+    data = request.json
+    name = data['name']
+    email = data['email']
+    contact = data['contact']
+
+    manager_collection = db['portfolio_Manager']
