d38cfc854fbc1667ee0646b01a6fd1e2695adbe4
NehaS101 <solankineha297@gmail.com>
2023-07-20 14:33:19 +0530

    create manager,get manager and get manager by id done

--- backend/controllers/manager_controller.py
+++ backend/controllers/manager_controller.py
@@ -16,3 +16,28 @@ def create_managers():
     contact = data['contact']
 
     manager_collection = db['portfolio_Manager']
+    manager_data = {
+        'name':name,
+        'email':email,
+        'contact':contact
+    }
+    manager_id = manager_collection.insert_one(manager_data).inserted_id
+
+    return jsonify({"message":"New Portfolio Manager created successfully"})
+
+def getAll_managers():
+    manager_collection = db['portfolio_Manager']
+    managers = list(manager_collection.find())
+    return jsonify(managers),200
+
+def get_managers_byId(manager_id):
+    manager_collection = db['portfolio_Manager']
+    manager = manager_collection.find_one({"_id":manager_id})
+
+    if manager:
+        return jsonify(manager),200
+    else:
+        return jsonify({"message":"Portfolio Manager not found"}),404
+    
+
+    
