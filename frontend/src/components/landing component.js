d9d5cac2d086374a005123836be9db46677bbefe
NehaS101 <solankineha297@gmail.com>
2023-07-21 15:40:10 +0530

    landing page css applied with content

--- frontend/src/components/landing component.js
+++ frontend/src/components/landing component.js
@@ -5,7 +5,7 @@ import "../modules/landing.css";
 const LandingComponent = () => {
   return (
     <div className="landing-page">
-      <div className="main-logo">
+      {/* <div className="main-logo">
         <div className="div1">
           <img src={require("../image/logo.png")} alt="logo" />
         </div>
@@ -16,32 +16,33 @@ const LandingComponent = () => {
             Manage your projects, tasks, and resources efficiently in one place.
           </h3>
         </div>
-      </div>
+      </div> */}
 
       <section className="hero-section">
         <div className="hero-content">
-          <h2 className="hero-title">Your Path to Project Success</h2>
+          <h1 className="hero-title">Your Path to Project Success</h1>
           <p className="hero-description">
             Are you looking to elevate your project management skills and
             streamline your portfolio like never before? Look no further!
-            PrismPort is your ultimate solution for efficient project
+            <b>PrismPort</b> is your ultimate solution for efficient project
             management, designed with your success in mind.
           </p>
+          <img className="hero-image" src={require("../image/background.jpg")} alt="hero" />
           <div className="cta-buttons">
             <Link to="/login">
               <button className="btn">Login</button>
             </Link>
             <Link to="/signup">
-              <button className="btn btn-secondary">Sign Up</button>
+              <button className="btn">Sign Up</button>
             </Link>
           </div>
         </div>
-        {/* Add your hero image here */}
+        
       </section>
+      
       <section className="features-section">
         <div className="feature">
-          {/* Add a feature image here */}
-          <h3 className="feature-title">Simplify Portfolio Management</h3>
+          <h2 className="feature-title">Simplify Portfolio Management</h2>
           <p className="feature-description">
             Seamlessly manage your portfolio of projects with our intuitive and
             user-friendly platform. Organize, track, and monitor your projects
@@ -49,8 +50,7 @@ const LandingComponent = () => {
           </p>
         </div>
         <div className="feature">
-          {/* Add a feature image here */}
-          <h3 className="feature-title">Efficient Task Management</h3>
+          <h2 className="feature-title">Efficient Task Management</h2>
           <p className="feature-description">
             Stay on top of your tasks with precision and ease. Assign resources,
             set deadlines, and track progress to ensure projects are executed
@@ -58,8 +58,7 @@ const LandingComponent = () => {
           </p>
         </div>
         <div className="feature">
-          {/* Add a feature image here */}
-          <h3 className="feature-title">Resource Allocation Made Easy</h3>
+          <h2 className="feature-title">Resource Allocation Made Easy</h2>
           <p className="feature-description">
             Allocate resources to tasks efficiently, optimizing productivity and
             maximizing results. PrismPort's resource management ensures you
@@ -67,8 +66,7 @@ const LandingComponent = () => {
           </p>
         </div>
         <div className="feature">
-          {/* Add a feature image here */}
-          <h3 className="feature-title">Advanced Project Analytics</h3>
+          <h2 className="feature-title">Advanced Project Analytics</h2>
           <p className="feature-description">
             Make data-driven decisions with confidence! Our comprehensive
             analytics provide valuable insights into your project performance,
