c4f569843713e52bb2869c62c20e5efaf30a48e0
NehaS101 <solankineha297@gmail.com>
2023-07-24 12:17:19 +0530

    resource entity frontend done

--- frontend/src/components/resourcecomponent.js
+++ frontend/src/components/resourcecomponent.js
@@ -1,7 +1,102 @@
-import React,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
+import ResourceService from '../services/resourceService';
 
-const ResourceComponent=()=>{
+const ResourceComponent = () => {
+  const [resources, setResources] = useState([]);
+  const [newResourceName, setNewResourceName] = useState("");
+  const [newTaskAssigned, setNewTaskAssigned] = useState("");
 
-}
+  useEffect(() => {
+    fetchResources();
+  }, []);
 
-export default ResourceComponent
+  const fetchResources = async () => {
+    try {
+      const data = await ResourceService.getAllResources();
+      console.log(data);
+      setResources(data);
+      
+    } catch (error) {
+      console.error("Error fetching resources:", error);
+    }
+  };
+
+  const handleCreateResource = async () => {
+    try {
+      const newResource = {
+        resource_name: newResourceName,
+        task_assigned: newTaskAssigned,
+      };
+
+      await ResourceService.createResource(newResource);
+
+      // Clear form fields after successful creation
+      setNewResourceName(newResource.resource_name);
+      setNewTaskAssigned(newResource.task_assigned);
+
+      // Refresh the list of resources
+      fetchResources();
+    } catch (error) {
+      console.error("Error creating resource:", error);
+    }
+  };
+
+  const handleDeleteResource = async (resourceId) => {
+    try {
+      await ResourceService.deleteResource(resourceId);
+
+      // Refresh the list of resources after successful deletion
+      fetchResources();
+    } catch (error) {
+      console.error("Error deleting resource:", error);
+    }
+  };
+
+  return (
+    <div className="resource">
+      <h2>Resources</h2>
+      <div className="create-resource">
+        <input
+          type="text"
+          value={newResourceName}
+          onChange={(e) => setNewResourceName(e.target.value)}
+          placeholder="Resource Name"
+        />
+        <input
+          type="text"
+          value={newTaskAssigned}
+          onChange={(e) => setNewTaskAssigned(e.target.value)}
+          placeholder="Task Assigned"
+        />
+        <button onClick={handleCreateResource}>Create Resource</button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Resource Name</th>
+            <th>Tasks Assigned</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+        {Array.isArray(resources) &&
+          resources.map((resource) => (
+            <tr key={resource._id.$oid}>
+              <td>{resource.resource_name}</td>
+              <td>
+                {resource.task_assigned}
+              </td>
+              <td>
+                <button onClick={() => handleDeleteResource(resource._id.$oid)}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+      </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ResourceComponent;
---
+++ frontend/src/services/resourceService.js
@@ -0,0 +1,54 @@
+const Base_URL = "http://127.0.0.1:5000/resource"; // Replace with your backend URL
+
+const ResourceService = {
+  getAllResources: async () => {
+    try {
+      const response = await fetch(`${Base_URL}/get_resource`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch resources.");
+      }
+      const data = await response.json();
+      console.log(data)
+      return JSON.parse(data);
+    } catch (error) {
+      throw new Error("Error fetching resources:", error);
+    }
+  },
+
+  createResource: async (newResource) => {
+    try {
+      const response = await fetch(`${Base_URL}/create_resource`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newResource),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to create resource.");
+      }
+      const data = await response.json();
+      alert(data.message)
+      return data;
+    } catch (error) {
+      throw new Error("Error creating resource:", error);
+    }
+  },
+
+  deleteResource: async (resourceId) => {
+    try {
+      const response = await fetch(`${Base_URL}/delete_resource/${resourceId}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error("Failed to delete resource.");
+      }
+      const data = await response.json();
+      alert(data.message);
+    } catch (error) {
+      throw new Error("Error deleting resource:", error);
+    }
+  },
+};
+
+export default ResourceService;
